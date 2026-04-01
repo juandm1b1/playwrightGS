cb62d737852e6237ad7889867c130e65b6793b93
juandm1b1 <juandm1b1@gmail.com>
2026-04-01 09:29:45 -0500

    CI actions 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class App {
     public static void main(String[] args) {
          try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
             Page page = browser.newPage();
             page.navigate("https://playwright.dev");
 
--- src/test/java/org/example/JUnit.java
+++ src/test/java/org/example/JUnit.java
@@ -23,7 +23,7 @@ public class JUnit {
   @BeforeAll
   static void launchBrowser() {
     playwright = Playwright.create();
-    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    browser = playwright.chromium().launch();
   }
 
   @AfterAll
